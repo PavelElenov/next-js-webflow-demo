549958cb3d2f56115233695fe940c2f58fe91c60
Pavel Elenov <pavelelenov@Pavels-Mac-mini.local>
2023-12-30 13:04:16 +0200

    change component props

--- src/app/components/findTheRighPrice/page.tsx
+++ src/app/components/findTheRighPrice/page.tsx
@@ -1,4 +1,4 @@
-import PriceCard from "../priceCard/page";
+import PriceCard, { IPriceCardData } from "../priceCard/page";
 import "./style.css";
 
 export default function FindTheRightPrice() {
@@ -8,26 +8,34 @@ export default function FindTheRightPrice() {
             <p className="find-the-right-price-text">Explore our flexible pricing options designed to meet the diverse needs of both job seekers and employers. Unlock the full potential of the Visual Developers Job Board and invest in your future success with our affordable and value-driven plans.</p>
             <div className="price-cards-wrapper">
                 <div className="price-card-wrapper">
-                    <PriceCard priceCardHeader="Employers"
-                        pricePerMonth={95}
-                        priceCardText="Our Employers Plan is tailored to provide maximum visibility and engagement for your job listings, ensuring you reach the most talented and qualified candidates in the development community."
-                        priceCardOptions={[
-                            "Priority placement of your job listings, resulting in increased visibility",
-                            "Customizable job posts with your company logo and branding",
-                            "Access to our curated pool of highly skilled Webflow, no-code, and traditional developers"]}
+                    <PriceCard params={{
+                        priceCardHeader: "Employers",
+                        pricePerMonth: 95,
+                        priceCardText: "Our Employers Plan is tailored to provide maximum visibility and engagement for your job listings, ensuring you reach the most talented and qualified candidates in the development community.",
+                        priceCardOptions:
+                            [
+                                "Priority placement of your job listings, resulting in increased visibility",
+                                "Customizable job posts with your company logo and branding",
+                                "Access to our curated pool of highly skilled Webflow, no-code, and traditional developers"
+                            ]
+                    }}
                     ></PriceCard>
                 </div>
                 <div className="price-card-wrapper">
-                    <PriceCard priceCardHeader="Employers"
-                        pricePerMonth={95}
-                        priceCardText="Our Employers Plan is tailored to provide maximum visibility and engagement for your job listings, ensuring you reach the most talented and qualified candidates in the development community."
-                        priceCardOptions={[
-                            "Priority placement of your job listings, resulting in increased visibility",
-                            "Customizable job posts with your company logo and branding",
-                            "Access to our curated pool of highly skilled Webflow, no-code, and traditional developers"]}
+                    <PriceCard params={{
+                        priceCardHeader: "Employers",
+                        pricePerMonth: 95,
+                        priceCardText: "Our Employers Plan is tailored to provide maximum visibility and engagement for your job listings, ensuring you reach the most talented and qualified candidates in the development community.",
+                        priceCardOptions:
+                            [
+                                "Priority placement of your job listings, resulting in increased visibility",
+                                "Customizable job posts with your company logo and branding",
+                                "Access to our curated pool of highly skilled Webflow, no-code, and traditional developers"
+                            ]
+                    }}
                     ></PriceCard>
                 </div>
             </div>
-        </section>
+        </section >
     );
 }
--- src/app/components/priceCard/page.tsx
+++ src/app/components/priceCard/page.tsx
@@ -1,14 +1,25 @@
+"use client";
+
 import "./style.css";
 
-export default function PriceCard(props: { priceCardHeader: string, pricePerMonth: number, priceCardText: string, priceCardOptions: string[] }) {
+export interface IPriceCardData {
+
+    priceCardHeader: string;
+    pricePerMonth: number;
+    priceCardText: string;
+    priceCardOptions: string[]
+
+}
+
+export default function PriceCard({ params, searchParams }: { params: IPriceCardData, searchParams?: string | undefined }) {
 
     return (
         <div className="price-card">
-            <h2 className="price-card-header">{props.priceCardHeader}</h2>
-            <p className="price-card-price-per-month"><span className="price-per-month">${props.pricePerMonth}</span>/month</p>
-            <p className="price-card-text">{props.priceCardText}</p>
+            <h2 className="price-card-header">{params.priceCardHeader}</h2>
+            <p className="price-card-price-per-month"><span className="price-per-month">${params.pricePerMonth}</span>/month</p>
+            <p className="price-card-text">{params.priceCardText}</p>
             <ul>
-                {props.priceCardOptions.map(option => <li key={option} className="price-card-option">{option}</li>)}
+                {params.priceCardOptions.map(option => <li key={option} className="price-card-option">{option}</li>)}
             </ul>
             <button className="price-card-button">Sign up</button>
         </div>
